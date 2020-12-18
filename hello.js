bfe116f1b6d041c1861cf50cc0e5c82468c75fca
Valentina <bucurc42@gmail.com>
2020-12-18 20:50:44 +0200

    HeloJS

---
+++ hello.js
@@ -0,0 +1,24 @@
+var time = 3000
+time = time + 1000
+var message = "Bine ai venit!"
+var isLate = true
+
+setTimeout(welcome,time)
+function welcome() {
+    alert(message)
+alert("Welcome!")
+    
+}
+
+if (isLate) {
+    alert("Go home")
+    
+}
+
+
+if (isLate) {
+    alert("Go home")
+    
+} else {
+    alert("Continue")
+}
